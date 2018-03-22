068742d3ae02aae45dd0f0f8b6edfc3c5981cb1c
Lewis Pearson <lewisp6@gmail.com>
2018-03-22 19:01:47 +0000

    Added second regex format to match.

--- src/extract.js
+++ src/extract.js
@@ -1,9 +1,33 @@
 const extract = {};
 
 extract.extractSteps = function extractStep(code) {
-  const regEx = /\(\/\^(.*?)\$/g;
-  const extractedArray = regEx.exec(code);
-  return extractedArray;
+  let slashFormatMatches = [];
+  let quoteFormatMatches = [];
+  let matches = [];
+
+  const slashFormat = /\(\/\^(.*?)\$/g;
+  slashFormatMatches = slashFormat.exec(code);
+
+  const quoteFormat = /\('(.*?)\'/g;
+  quoteFormatMatches = quoteFormat.exec(code);
+
+  if (matchedSlashFormat(slashFormatMatches) && matchedQuoteFormat(quoteFormatMatches)) {
+    matches = slashFormatMatches.concat(quoteFormatMatches);
+  } else if (matchedSlashFormat(slashFormatMatches)) {
+    matches = slashFormatMatches;
+  } else if (matchedQuoteFormat(quoteFormatMatches)) {
+    matches = quoteFormatMatches;
+  }
+
+  return matches;
+}
+
+function matchedSlashFormat(slashFormatMatches) {
+ return slashFormatMatches !== null;
+}
+
+function matchedQuoteFormat(quoteFormatMatches) {
+  return quoteFormatMatches !== null;
 }
 
 module.exports = extract;
--- test/extract.spec.js
+++ test/extract.spec.js
@@ -8,7 +8,27 @@ describe('Extract step from step definition', function() {
       '(/^the response status is (.*)$',
       'the response status is (.*)'
     ];
+
     const extractedSteps = extract.extractSteps(exampleCode);
     assert.deepEqual(expected, extractedSteps);
+  });
+
+  it ('should return the gherkin text from single quote format', function() {
+    const exampleCode = 'Then(\'the first result is {stringInDoubleQuotes}\', function() {});';
+    const expected = [
+      '(\'the first result is {stringInDoubleQuotes}\'',
+      'the first result is {stringInDoubleQuotes}'
+    ];
+
+    const extractedSteps = extract.extractSteps(exampleCode);
+    assert.deepEqual(expected, extractedSteps);
+  });
+
+  it ('should return matches for both syntaxes', function() {
+    const exampleCode = 'Then(\'the first result is {stringInDoubleQuotes}\', function() {}Then(/^the response status is (.*)$/, function (status) {assert.equal(this.responseStatus, status)});'
+    const expected = [
+      '(\'the first result is {stringInDoubleQuotes}\'',
+      '(/^the response status is (.*)$'
+    ];
   });
 });
